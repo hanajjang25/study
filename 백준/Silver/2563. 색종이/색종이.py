f260764d92d10ecb6bc705e6529e1dc01b4cf686
hanajjang25 <93505567+hanajjang25@users.noreply.github.com>
2025-02-27 13:07:12 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+paper = [[0]*100 for i in range(100)]
+
+for _ in range(num):
+  x, y = map(int, input().split())
+  for i in range(x, x+10):
+    for j in range(y, y+10):
+      paper[i][j] = 1
+
+total = 0
+
+for k in range(100):
+  total += paper[k].count(1)
+
+print(total)
